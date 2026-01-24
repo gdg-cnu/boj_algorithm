2481209be1494844698f89f9db65b8ad69e9e9a0
ben7094 <rlatkdgns7094@gmail.com>
2026-01-24 15:51:32 +0900

    BOJ-EX: 1/24/2026, 3:51:31 PM

--- sanghun/practice.py
+++ sanghun/practice.py
@@ -1,82 +1,13 @@
-from collections import deque
 import sys
-input = sys.stdin.readline
 
+N = int(sys.stdin.readline())
+count = [0] * 1001
+numbers = list(map(int, sys.stdin.readline().split()))
 
-N, M, Start = map(int, input().split())
+for number in numbers:
+  count[number] += 1
 
-A = [[] * (N+1)]
-visited = [[False] * (N+1)]
-
-for _ in range(M):
-  s, e = map(int, input().split())
-  A[s].append(e)
-  A[e].append(s)
-
-for i in range(N+1):
-  A[i].sort()
-
-def DFS(v):
-  print(v, end= ' ')
-  visited[v] = True
-  for i in A[v]:
-    if not visited[i]:
-      visited[i] = True
-      DFS(i)
-
-
-def DFS(v):
-  print(v, end = ' ')
-  visited[v] = True
-  for i in A[v]:
-    if not visited[i]:
-      visited[i] = True
-      DFS(i)
-
-
-def DFS(v):
-  print(v, end = ' ')
-  visited[v] = True
-  for i in A[v]:
-    if not visited[i]:
-      visited[i] = True
-      DFS(i)
-
-
-
-def BFS(v):
-  queue = deque()
-  visited[v] = True
-  queue.append(v)
-  while queue:
-    now_Node = queue.popleft()
-    print(now_Node, end = ' ')
-    # 여기 방금 한거 헷갈리지 말자! 왜냐면 그 다음 while문 계속 돌리려면 이렇게 해줘야한다
-    for i in A[now_Node]:
-      if not visited[i]:
-        visited[i] = True
-        queue.append(i)
-
-
-
-
-
-
-def BFS(v):
-  queue = deque()
-  visited[v] = True
-  queue.append(v)
-  while queue:
-    now_Node = queue.popleft()
-    print(now_Node, end = ' ')
-    for i in A[now_Node]:
-      if not visited[i]:
-        visited[i] = True
-        queue.append(i)
-print()
-visited = [False] * (N+1)
-BFS(Start)
-
-
-D = [[0 for j in range(2)] for i in range(N+1)]
-
+for i in range(1001):
+  if count[i] != 0:
+    for _ in range(count[i]):
+      sys.stdout.write(str(i) + ' ')
